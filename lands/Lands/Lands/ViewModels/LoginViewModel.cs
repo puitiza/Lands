ece73d7f7aa6f9ee105fcb6578840933562e54f9
Puitiza <anthony.puitiza.02@gmail.com>
2018-10-15 17:31:01 -0500

    Patron MVVM
     se observa este patron en el login

---
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        //public bool IsEnabled
+        //{
+        //    get { return this.isEnabled; }
+        //    set { SetValue(ref this.isEnabled, value); }
+        //}
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+            //this.Email = "jzuluaga55@hotmail.com";
+            //this.Password = "123456";
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+            
+        }
+        #endregion
+    }
+}
--- lands/Lands/Lands/ViewModels/MainViewModel.cs
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -2,6 +2,21 @@
 {
     class MainViewModel
     {
+        //Esta clase es el principal coordinador de los ViewModels y dentro de estos hacemos referencia a cada uno de los VieModels
 
+        #region ViewModels
+        //Dentro del ScrollView en LoginPage hay un BindingContext="{Binding Login} y este mismo nombre esta acá en el Main
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
